c4c7c22d319e77ce454ab279dd96986da9eaf688
Senthil Kumaran <senthil.kumaran@okta.com>
2016-01-08 14:01:12 -0800

    Baconize okta java sdk.
    OKTA-77717
    <<<Jenkins Check-In of Tested SHA: 967f8d5cda8d328d10d449ee29faf5df475db3b9 for senthil.kumaran@okta.com>>> 
    Artifact: SDK.Java

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
- 
-# Use the 1.7 JDK
-JAVA_HOME=${JAVA7_HOME}
-export PATH=${PATH}:${JAVA_HOME}/bin
+set -e +x
 
-# Define the location of artifactory
-export ARTIFACTORY_URL=https://artifacts.aue1d.saasure.com/artifactory
- 
+PROJECT=${REPO}
+
+# Use the 1.7 JDK
+export PATH=${PATH}:${JAVA7_HOME}/bin
+
+MAVEN=${OKTA_HOME}/thirdparty/maven-current/bin/mvn
+MAVEN_SETTINGS=${OKTA_HOME}/jenkins-tasks/data/maven/settings.xml
+
 # This is the Aperture test_suite_id for the TestSuite we want to start
 BUILD_AND_TEST_TEST_SUITE_ID=771f3f55-7db9-11e5-80ff-22000b5181e9
  
@@ -15,7 +17,7 @@ start_test_suite ${BUILD_AND_TEST_TEST_SUITE_ID}
  
 # Run the build and tests and deploy, don't fail on error
 function build_and_test() {
-  mvn -Pci-run clean deploy -Dmaven.test.failure.ignore=true -s ${MAVEN_SETTINGS}
+  ${MAVEN} clean deploy -s "${MAVEN_SETTINGS}"
 }
  
 # Catch compilation errors and log to s3 (fail/finish_test_suite will do this)
@@ -23,4 +25,5 @@ if ! build_and_test; then
   fail_test_suite
   exit 1
 fi
-finish_test_suite build
+
+finish_test_suite "junit" "${PROJECT}/target/surefire-reports/"
