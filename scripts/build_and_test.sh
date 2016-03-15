8901ebf38a8cee4c6b089b47a212d9412f3cc86f
Senthil Kumaran <senthil@uthcode.com>
2016-03-15 12:01:29 -0700

    Set JAVA_HOME and remove extraneous comments.

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -3,8 +3,8 @@ set -e +x
 
 PROJECT=${REPO}
 
-# Use the 1.7 JDK
 export PATH=${PATH}:${JAVA7_HOME}/bin
+export JAVA_HOME=${JAVA7_HOME}
 
 MAVEN=${OKTA_HOME}/thirdparty/maven-current/bin/mvn
 MAVEN_SETTINGS=${OKTA_HOME}/jenkins-tasks/data/maven/settings.xml
@@ -12,15 +12,12 @@ MAVEN_SETTINGS=${OKTA_HOME}/jenkins-tasks/data/maven/settings.xml
 # This is the Aperture test_suite_id for the TestSuite we want to start
 BUILD_AND_TEST_TEST_SUITE_ID=771f3f55-7db9-11e5-80ff-22000b5181e9
  
-# Mark the start of our TestSuite
 start_test_suite ${BUILD_AND_TEST_TEST_SUITE_ID}
  
-# Run the build and tests and deploy, don't fail on error
 function build_and_test() {
   ${MAVEN} clean deploy -s "${MAVEN_SETTINGS}"
 }
  
-# Catch compilation errors and log to s3 (fail/finish_test_suite will do this)
 if ! build_and_test; then
   fail_test_suite
   exit 1
