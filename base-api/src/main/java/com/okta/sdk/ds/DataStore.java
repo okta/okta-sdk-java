c92af90c23e8757a1ab2764b8151b278a280f919
Brian Demers <bdemers@apache.org>
2017-05-01 14:24:49 -0400

    Add initial base-api
    This module’s name may change in the near future

---
+++ base-api/src/main/java/com/okta/sdk/ds/DataStore.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2017 Okta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.sdk.ds;
+
+import com.okta.sdk.cache.CacheManager;
+import com.okta.sdk.query.Options;
+import com.okta.sdk.resource.Resource;
+
+/**
+ * A {@code DataStore} is the liaison between client SDK components and the raw Okta REST API.  It is responsible
+ * for converting SDK objects (Account, Directory, Group instances, etc) into REST HTTP requests, executing those
+ * requests, and converting REST HTTP responses back into SDK objects.
+ *
+ * @since 1.0.0
+ */
+public interface DataStore {
+
+    /**
+     * Instantiates and returns a new instance of the specified Resource type.  The instance is merely instantiated and
+     * is not saved/synchronized with the server in any way. <p> This method effectively replaces the {@code new}
+     * keyword that would have been used otherwise if the concrete implementation was known (Resource implementation
+     * classes are intentionally not exposed to SDK end-users).
+     *
+     * @param clazz the Resource class to instantiate.
+     * @param <T>   the Resource sub-type
+     * @return a new instance of the specified Resource.
+     */
+    <T extends Resource> T instantiate(Class<T> clazz);
+
+    /**
+     * Looks up (retrieves) the resource at the specified {@code href} URL and returns the resource as an instance of
+     * the specified {@code class}. <p> The {@code Class} argument must represent an interface that is a sub-interface
+     * of {@link Resource}.
+     *
+     * @param href  the resource URL of the resource to retrieve
+     * @param clazz the {@link Resource} sub-interface to instantiate
+     * @param <T>   type parameter indicating the returned value is a {@link Resource} instance.
+     * @return an instance of the specified class based on the data returned from the specified {@code href} URL.
+     */
+    <T extends Resource> T getResource(String href, Class<T> clazz);
+
+    /**
+     * Retrieves the resource at the specified {@code href} according to the specified {@code Options} and returns the
+     * resource as an instance of the specified {@code clazz}.
+     *
+     * @param href  the URL of the resource to retrieve
+     * @param clazz the {@link Resource} sub-interface to instantiate
+     * @param <T>   type parameter indicating the returned value is a {@link Resource} instance.
+     * @param options the {@link Options} sub-interface with the properties to expand
+     * @return an instance of the specified class based on the data returned from the specified {@code href} URL.
+     * @since 1.0.RC4.6
+     */
+    <T extends Resource, O extends Options> T getResource(String href, Class<T> clazz, O options);
+
+    /**
+     * Returns the CacheManager used to improve data store performance.
+     *
+     * @return the CacheManager used to improve data store performance.
+     * @since 1.0.RC3
+     */
+    CacheManager getCacheManager();
+
+}
---
+++ base-api/src/main/java/com/okta/sdk/resource/CollectionResource.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2017 Okta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.sdk.resource;
+
+/**
+ * A {@code CollectionResource} is a first-class {@link Resource} that has its own properties (such as
+ * {@link #getOffset() offset} and {@link #getLimit() limit}, but also contains other {@link Resource} instances.
+ *
+ * @since 1.0.0
+ */
+public interface CollectionResource<T extends Resource> extends Resource, Iterable<T> {
+
+    /**
+     * <p>
+     * Returns the index of the resource in the entire collection that is the first resource in the current page.
+     * </p><p>
+     * All subsequent resources in the response page will immediately follow this index.
+     * </p><p>
+     * For example, a paged query with an offset of 50 will return a page of results where the page contains the
+     * resource at index 50, then the resource at index 51, then at 52, 53, etc.
+     * </p>
+     * If unspecified when retrieving the collection, this number defaults to {@code 0}, indicating that the results
+     * should start at the first result in the overall set (i.e the first 'page').  The maximum number of elements
+     * returned in a page is specified by the {@link #getLimit() limit}.
+     *
+     * @return the index of the resource in the entire collection that is the first resource in the current page.
+     */
+    public int getOffset();
+
+    /**
+     * Returns the maximum number of results included in a single page.  If unspecified when retrieving the collection,
+     * this number defaults to {@code 25}.  The minimum number allowed is {@code 1}, the maximum is {@code 100}.
+     *
+     * @return the maximum number of results included in a single page. Min: 1, Max: 100.
+     */
+    public int getLimit();
+
+    /**
+     * Returns the total count (quantity) of all items across the entire collection. Pagination does not impact this value.
+     * <p>The <code>size</code> property does not necessary express that all these items have been transferred from the backend.
+     * This value indicates the total quantity of items that the list contains but, in order to avoid retrieving a large
+     * amount of data at once from the backend, the list is actually paginated with the size specified by <code>limit</code>.
+     * This means, that for example, if a list has a <code>size</code> of 100 and the <code>limit</code> is 25, the backend
+     * will be contacted four times and 25 items will be retrieved each time.</p>
+     *
+     * @return the total quantity of items that this CollectionResource contains.
+     * @since 1.0.RC4
+     */
+    public int getSize();
+
+    /**
+     * This is just a convenience method to retrieve the single element expected to exist in this collection. This method is
+     * intended to be used in cases where the returned list is explicitly expected to contain a single element. This operation will throw
+     * an exception if this list contains zero or more than one element.
+     *
+     * <p>This method is not backed by any specific property obtained from the backend, as already mentioned, this is a convenience method.</p>
+     *
+     * @return the single unique resource that is expect to be contained within this list.
+     * @throws java.lang.IllegalStateException if this list contains either zero or more than one element.
+     * @since 1.0.RC4.4
+     */
+    public T single();
+
+}
