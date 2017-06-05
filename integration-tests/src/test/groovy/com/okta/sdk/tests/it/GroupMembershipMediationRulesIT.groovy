0d531843290d818d1db560ddeaddf3df05ff6cfb
Brian Demers <bdemers@apache.org>
2017-06-05 12:36:14 -0400

    Add basic CRUD IT for Group Membership Rules

---
+++ integration-tests/src/test/groovy/com/okta/sdk/tests/it/GroupMembershipMediationRulesIT.groovy
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2017 Okta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.sdk.tests.it
+
+import com.okta.sdk.client.Client
+import com.okta.sdk.resource.AssignUserToGroupsMediationAction
+import com.okta.sdk.resource.GroupBuilder
+import com.okta.sdk.resource.GroupMembershipMediationActions
+import com.okta.sdk.resource.GroupMembershipMediationConditions
+import com.okta.sdk.resource.GroupMembershipMediationExpressionCondition
+import com.okta.sdk.resource.GroupMembershipMediationRule
+import com.okta.sdk.resource.group.UserGroup
+
+/**
+ * Tests for /api/v1/groups/rules
+ * @since 0.5.0
+ */
+class GroupMembershipMediationRulesIT implements CrudTestSupport {
+
+    UserGroup group
+
+    void preTestSetup(Client client) {
+        group = GroupBuilder.INSTANCE
+                .setName("my-user-group-" + UUID.randomUUID().toString())
+                .setDescription("IT created Group")
+                .buildAndCreate(client)
+    }
+
+    @Override
+    def create(Client client) {
+
+        GroupMembershipMediationRule rule = client.instantiate(GroupMembershipMediationRule)
+        rule.setName("rule+" + UUID.randomUUID().toString() )
+        rule.setType("group_rule")
+        rule.setStatus(GroupMembershipMediationRule.StatusEnum.ACTIVE)
+
+        rule.setConditions(client.instantiate(GroupMembershipMediationConditions))
+        rule.getConditions().setExpression(client.instantiate(GroupMembershipMediationExpressionCondition))
+        rule.getConditions().getExpression().setValue("user.firstName==\"Joe\"")
+
+        rule.setActions(client.instantiate(GroupMembershipMediationActions))
+        rule.getActions().setAssignUserToGroups(client.instantiate(AssignUserToGroupsMediationAction))
+        rule.getActions().getAssignUserToGroups().setGroupIds(Collections.singletonList(group.id))
+
+        return client.createRule(rule)
+    }
+
+    @Override
+    def read(Client client, String id) {
+        return client.getRule(id)
+    }
+
+    @Override
+    void update(Client client, def rule) {
+        rule.setName(rule.name +"-2")
+        rule.update()
+    }
+
+    @Override
+    void delete(Client client, def rule) {
+        rule.delete()
+    }
+
+    @Override
+    Iterator getResourceCollectionIterator(Client client) {
+        return client.listRules().iterator()
+    }
+}
