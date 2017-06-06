f659b31bf7c04a02b58b9ee815218f3e68b5bff3
Brian Demers <bdemers@apache.org>
2017-06-06 13:03:15 -0400

    Adding release script to src/ci

--- src/ci/build.sh
+++ src/ci/build.sh
@@ -15,34 +15,10 @@
 # limitations under the License.
 #
 
+set -e
 
-# deploy snapshot from ONLY this branch
-SNAPSHOT_BRANCH="master"
-
-# Get the slug from the TRAVIS var, or parse the 'origin' remote
-REPO_SLUG=${REPO_SLUG:-${TRAVIS_REPO_SLUG:-$(git remote get-url origin | sed 's_.*\:__; s_.*github.com/__; s_\.git__')}}
-PULL_REQUEST=${PULL_REQUEST:-${TRAVIS_PULL_REQUEST:-true}} # default to true
-BRANCH=${TRAVIS_BRANCH:-"$(git rev-parse --abbrev-ref HEAD)"}
-
-# run the ITs if we have an orgUrl
-if [ ! -z $OKTA_APICLIENT_ORGURL ] ; then
-    RUN_ITS=true
-fi
-RUN_ITS=${RUN_ITS:-false}
-
-# we only deploy from a given branch NOT for pull requests, and ONLY when we can run the ITs
-# and do NOT deploy releases, only snapshots right now
-if [ "$BRANCH" = "$SNAPSHOT_BRANCH" ] && [ "$PULL_REQUEST" = false ] && [ "$RUN_ITS" = true ] && [ ! "$IS_RELEASE" = true ]; then
-        DEPLOY=true
-fi
-DEPLOY=${DEPLOY:-false}
-
-# print the props so it is easier to debug on Travis or locally.
-echo "REPO_SLUG: ${REPO_SLUG}"
-echo "PULL_REQUEST: ${PULL_REQUEST}"
-echo "BRANCH: ${BRANCH}"
-echo "IS_RELEASE: ${IS_RELEASE}"
-echo "RUN_ITS: ${RUN_ITS}"
+COMMON_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common.sh"
+source "${COMMON_SCRIPT}"
 
 # all the prep is done, lets run the build!
 MVN_CMD="mvn -s src/ci/settings.xml"
