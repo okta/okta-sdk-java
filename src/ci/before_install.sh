04a429f244c1fe90226aef3edfa78de83837c8bd
Brian Demers <bdemers@apache.org>
2017-06-06 15:49:51 -0400

    Move Maven installation up one directory

--- src/ci/before_install.sh
+++ src/ci/before_install.sh
@@ -20,9 +20,11 @@
 export ARTIFACT_VERSION="$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml)"
 export IS_RELEASE="$([ ${ARTIFACT_VERSION/SNAPSHOT} == $ARTIFACT_VERSION ] && [ $TRAVIS_BRANCH == 'master' ] && echo 'true')"
 #Install Maven 3.3.9 since Travis uses 3.2 by default
-wget http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
-unzip -qq apache-maven-3.3.9-bin.zip
-export M2_HOME=$PWD/apache-maven-3.3.9
+MVN_VERSION=3.3.9
+wget http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/${MVN_VERSION}/binaries/apache-maven-${MVN_VERSION}-bin.zip
+unzip -qq apache-maven-${MVN_VERSION}-bin.zip -d ..
+rm apache-maven-${MVN_VERSION}-bin.zip
+export M2_HOME=$PWD/../apache-maven-${MVN_VERSION}
 export PATH=$M2_HOME/bin:$PATH
 
 info "Build configuration:"
