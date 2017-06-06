6e6700806270748a9708f3c8c0cee06403d8537f
Brian Demers <bdemers@apache.org>
2017-06-06 13:03:15 -0400

    Adding release script to src/ci

---
+++ src/ci/release.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright 2017 Okta
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+COMMON_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common.sh"
+source "${COMMON_SCRIPT}"
+
+OLD_VERSION="$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml)"
+NEW_VERSION="${OLD_VERSION/-SNAPSHOT}"
+
+# TODO: we must use our local maven settings file as this script is NOT ready for triggered by travis
+# GPG agent configuration needed to sign artifacts
+MVN_CMD="mvn"
+
+# Update pom versions, tag, update to new dev version
+${MVN_CMD} release:prepare --batch-mode
+
+# stage the release artifacts
+${MVN_CMD} release:perform
+
+# publish once to the versioned dir
+$MVN_CMD javadoc:aggregate scm-publish:publish-scm -Ppub-docs -Djavadoc.version.dir=''
+# and again to the unversioned dir
+$MVN_CMD javadoc:aggregate scm-publish:publish-scm -Ppub-docs -Djavadoc.version.dir="${NEW_VERSION}/"
+
+#notify for new release
+send_tag_notification "okta-sdk-root-${NEW_VERSION}"
