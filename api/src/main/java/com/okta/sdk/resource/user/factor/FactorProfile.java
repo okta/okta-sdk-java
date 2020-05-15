0ab5e55cbd82b4acf628e172cb7f7dbf23342c77
Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>
2020-05-14 23:17:39 -0700

    added license header and removed unused imports

--- api/src/main/java/com/okta/sdk/resource/user/factor/FactorProfile.java
+++ api/src/main/java/com/okta/sdk/resource/user/factor/FactorProfile.java
@@ -1,4 +1,19 @@
 /*
+ * Copyright 2018-Present Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
  * Okta API
  * Allows customers to easily access the Okta API
  *
@@ -10,16 +25,9 @@
  * Do not edit the class manually.
  */
 
-
 package com.okta.sdk.resource.user.factor;
 
-
-
-import java.util.Map;
 import com.okta.sdk.resource.ExtensibleResource;
-import com.okta.sdk.resource.Deletable;
-import com.okta.sdk.resource.Saveable;
-
 
 /**
  * FactorProfile
@@ -29,4 +37,3 @@ public interface FactorProfile extends ExtensibleResource {
 
 
 }
-
