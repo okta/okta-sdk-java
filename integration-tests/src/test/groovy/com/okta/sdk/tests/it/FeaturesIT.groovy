2e96b108c665262e71a97c735c8e032dd17ecec5
Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>
2020-06-07 23:06:07 -0700

    removed the enable/disable feature test due to its fragility

--- integration-tests/src/test/groovy/com/okta/sdk/tests/it/FeaturesIT.groovy
+++ integration-tests/src/test/groovy/com/okta/sdk/tests/it/FeaturesIT.groovy
@@ -15,14 +15,10 @@
  */
 package com.okta.sdk.tests.it
 
-import com.okta.sdk.resource.ResourceException
-import com.okta.sdk.resource.common.EnabledStatus
 import com.okta.sdk.resource.feature.Feature
 import com.okta.sdk.resource.feature.FeatureList
 import com.okta.sdk.tests.it.util.ITSupport
-import org.testng.Assert
 import org.testng.annotations.Test
-import wiremock.org.apache.commons.lang3.RandomStringUtils
 
 import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.*
@@ -37,29 +33,17 @@ class FeaturesIT extends ITSupport {
     void featureOperationsTest() {
         // list features
         FeatureList featureList = client.listFeatures()
-        assertThat(featureList, iterableWithSize(greaterThanOrEqualTo(1)))
+        assertThat(featureList, iterableWithSize(greaterThan(0)))
 
-        // get first feature from the list (size >= 1)
+        // pick first one from the list
         Feature feature = featureList.getAt(0)
-        assertThat(feature.id, notNullValue())
+        assertThat(feature, notNullValue())
 
-        EnabledStatus enabledStatus = feature.getStatus()
-
-        // update a feature (lifecycle)
-        if (enabledStatus == EnabledStatus.ENABLED) {
-            // disable it
-            // "force" ensures all dependencies are also updated, if any
-            feature.updateLifecycle("false", "force")
-            assertThat(feature.getStatus(), is(EnabledStatus.DISABLED))
-        } else {
-            // enabling it programmatically may not work for all features
-            // ref: https://developer.okta.com/docs/reference/api/features/#error-response-method-not-allowed
-            try {
-                feature.updateLifecycle("true", "force")
-                assertThat(feature.getStatus(), is(EnabledStatus.ENABLED))
-            } catch (ResourceException e) {
-                assertThat e.status, equalTo(405)
-            }
-        }
+        // get feature by id
+        Feature retrievedFeature = client.getFeature(feature.getId())
+        assertThat(retrievedFeature, notNullValue())
+        assertThat(retrievedFeature.getId(), equalTo(feature.getId()))
+        assertThat(retrievedFeature.getName(), equalTo(feature.getName()))
+        assertThat(retrievedFeature.getDescription(), equalTo(feature.getDescription()))
     }
 }
