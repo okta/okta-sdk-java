0bc11312c5ca8b320130ab753fb4b6db433b680b
Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>
2020-06-11 15:01:00 -0700

    review ready

--- integration-tests/src/test/groovy/com/okta/sdk/tests/it/IdpIT.groovy
+++ integration-tests/src/test/groovy/com/okta/sdk/tests/it/IdpIT.groovy
@@ -15,21 +15,17 @@
  */
 package com.okta.sdk.tests.it
 
-import com.okta.sdk.client.Client
 import com.okta.sdk.resource.*
 import com.okta.sdk.resource.identity.provider.IdentityProvider
 import com.okta.sdk.resource.policy.IdentityProviderPolicy
+import com.okta.sdk.resource.user.User
+import com.okta.sdk.resource.user.UserBuilder
 import com.okta.sdk.tests.it.util.ITSupport
 import org.testng.annotations.Test
 
-import static com.okta.sdk.tests.it.util.Util.assertPresent
-import static com.okta.sdk.tests.it.util.Util.assertNotPresent
-
+import static com.okta.sdk.tests.it.util.Util.*
 import static org.hamcrest.MatcherAssert.assertThat
-import static org.hamcrest.Matchers.equalTo
-import static org.hamcrest.Matchers.not
-import static org.hamcrest.Matchers.notNullValue
-import static org.hamcrest.Matchers.sameInstance
+import static org.hamcrest.Matchers.*
 
 /**
  * Tests for {@code /api/v1/idps}.
@@ -103,6 +99,10 @@ class IdpIT extends ITSupport {
         assertThat(createdIdp.getId(), notNullValue())
         assertThat(createdIdp.getStatus(), equalTo(IdentityProvider.StatusEnum.ACTIVE))
 
+        // get
+        IdentityProvider retrievedIdp = client.getIdentityProvider(createdIdp.getId())
+        assertThat(retrievedIdp.getId(), equalTo(createdIdp.getId()))
+
         // deactivate
         createdIdp.deactivate()
 
@@ -112,7 +112,110 @@ class IdpIT extends ITSupport {
         // earlier operation may not complete immediately, so sleep for 2s
         sleep(2000)
 
+        expect(ResourceException) {
+            client.getIdentityProvider(createdIdp.getId())
+        }
+
         assertNotPresent(client.listIdentityProviders(), createdIdp)
     }
 
+    @Test
+    void idpUserTest() {
+
+        // create user
+        def email = "joe.coder+${uniqueTestName}@example.com"
+        User createdUser = UserBuilder.instance()
+            .setEmail(email)
+            .setFirstName("Joe")
+            .setLastName("Code")
+            .setPassword("Password1".toCharArray())
+            .buildAndCreate(client)
+        registerForCleanup(createdUser)
+
+        // create idp
+        IdentityProvider createdIdp = client.createIdentityProvider(client.instantiate(IdentityProvider)
+            .setType(IdentityProvider.TypeEnum.OIDC)
+            .setName("Mock OpenID Connect Id")
+            .setIssuerMode(IdentityProvider.IssuerModeEnum.ORG_URL)
+            .setProtocol(client.instantiate(Protocol)
+                .setAlgorithms(client.instantiate(ProtocolAlgorithms)
+                    .setRequest(client.instantiate(ProtocolAlgorithmType)
+                        .setSignature(client.instantiate(ProtocolAlgorithmTypeSignature)
+                            .setAlgorithm("SHA-256")
+                            .setScope(ProtocolAlgorithmTypeSignature.ScopeEnum.REQUEST)))
+                    .setResponse(client.instantiate(ProtocolAlgorithmType)
+                        .setSignature(client.instantiate(ProtocolAlgorithmTypeSignature)
+                            .setAlgorithm("SHA-256")
+                            .setScope(ProtocolAlgorithmTypeSignature.ScopeEnum.ANY))))
+                .setEndpoints(client.instantiate(ProtocolEndpoints)
+                    .setAcs(client.instantiate(ProtocolEndpoint)
+                        .setBinding(ProtocolEndpoint.BindingEnum.POST)
+                        .setType(ProtocolEndpoint.TypeEnum.INSTANCE))
+                    .setAuthorization(client.instantiate(ProtocolEndpoint)
+                        .setBinding(ProtocolEndpoint.BindingEnum.REDIRECT)
+                        .setUrl("https://idp.example.com/authorize"))
+                    .setToken(client.instantiate(ProtocolEndpoint)
+                        .setBinding(ProtocolEndpoint.BindingEnum.POST)
+                        .setUrl("https://idp.example.com/token"))
+                    .setUserInfo(client.instantiate(ProtocolEndpoint)
+                        .setBinding(ProtocolEndpoint.BindingEnum.REDIRECT)
+                        .setUrl("https://idp.example.com/userinfo"))
+                    .setJwks(client.instantiate(ProtocolEndpoint)
+                        .setBinding(ProtocolEndpoint.BindingEnum.REDIRECT)
+                        .setUrl("https://idp.example.com/keys")))
+                .setScopes(["openid", "profile", "email"])
+                .setType(Protocol.TypeEnum.OIDC)
+                .setCredentials(client.instantiate(IdentityProviderCredentials)
+                    .setClient(client.instantiate(IdentityProviderCredentialsClient)
+                        .setClientId("your-client-id")
+                        .setClientSecret("your-client-secret")))
+                .setIssuer(client.instantiate(ProtocolEndpoint)
+                    .setUrl("https://idp.example.com")))
+            .setPolicy(client.instantiate(IdentityProviderPolicy)
+                .setAccountLink(client.instantiate(PolicyAccountLink)
+                    .setAction(PolicyAccountLink.ActionEnum.AUTO)
+                    .setFilter(null))
+                .setProvisioning(client.instantiate(Provisioning)
+                    .setAction(Provisioning.ActionEnum.AUTO)
+                    .setConditions(client.instantiate(ProvisioningConditions)
+                        .setDeprovisioned(client.instantiate(ProvisioningDeprovisionedCondition)
+                            .setAction(ProvisioningDeprovisionedCondition.ActionEnum.NONE))
+                        .setSuspended(client.instantiate(ProvisioningSuspendedCondition)
+                            .setAction(ProvisioningSuspendedCondition.ActionEnum.NONE)))
+                    .setGroups(client.instantiate(ProvisioningGroups)
+                        .setAction(ProvisioningGroups.ActionEnum.NONE)))
+                .setMaxClockSkew(120000)
+                .setSubject(client.instantiate(PolicySubject)
+                    .setUserNameTemplate(client.instantiate(PolicyUserNameTemplate)
+                        .setTemplate("idpuser.email"))
+                    .setMatchType(PolicySubjectMatchType.USERNAME))))
+        registerForCleanup(createdIdp)
+
+        // list linked idp users
+        assertThat(createdIdp.listUsers(), iterableWithSize(0))
+
+        // link user
+        IdentityProviderApplicationUser idpAppUser = createdIdp.linkUser(createdUser.getId(),
+            client.instantiate(UserIdentityProviderLinkRequest)
+                .setExternalId("externalId"))
+
+        assertThat(createdIdp.listUsers(), iterableWithSize(1))
+        assertPresent(createdIdp.listUsers(), idpAppUser)
+
+        // unlink user
+        createdIdp.unlinkUser(createdUser.getId())
+
+        // list linked idp users
+        assertThat(createdIdp.listUsers(), iterableWithSize(0))
+
+        // list social auth tokens
+        SocialAuthTokenList socialAuthTokenList = createdIdp.listSocialAuthTokens(createdUser.getId())
+        assertThat(socialAuthTokenList, iterableWithSize(0))
+
+        // deactivate
+        createdIdp.deactivate()
+
+        // delete
+        createdIdp.delete()
+    }
 }
