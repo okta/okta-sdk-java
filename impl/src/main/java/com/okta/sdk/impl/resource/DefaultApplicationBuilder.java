bd43772fa446b8f90a265ea0b8bb2cf58d51fc9e
Jeyadev Asokan <jeyadev.asokan@gmail.com>
2020-07-01 00:21:28 +0530

    Added an extendable ApplicationBuilder and created OIdCApplicationBuilder. (#360)
    * Created builder for OpenIdConnectApplication.
    
    * Created builder for OpenIdConnectApplication.
    
    * Fixing pmd/findbug failures.
    
    * Created builder for OpenIdConnectApplication.
    
    * Created builder for OpenIdConnectApplication.
    
    * Fixing pmd/findbug failures.
    
    * Fixing license check failures (that were missed).
    
    * Update ApplicationBuilder.java
    
    * Update OIdCApplicationBuilder.java
    
    * Update DefaultApplicationBuilder.java
    
    * Update DefaultOIdCApplicationBuilder.java
    
    * Update DefaultApplicationBuilderTest.groovy
    
    * Update DefaultOIdcApplicationBuilderTest.groovy
    
    * Update OIdCApplicationIT.groovy
    
    Co-authored-by: Jeyadev Asokan <jeyadev_asokan@baxter.com>
    Co-authored-by: Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>

---
+++ impl/src/main/java/com/okta/sdk/impl/resource/DefaultApplicationBuilder.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2020-Present Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.sdk.impl.resource;
+
+import com.okta.commons.lang.Strings;
+import com.okta.sdk.client.Client;
+import com.okta.sdk.resource.application.*;
+
+import java.util.Objects;
+
+public class DefaultApplicationBuilder<T extends ApplicationBuilder> implements ApplicationBuilder<T> {
+
+    protected String name;
+    protected String label;
+    protected String errorRedirectUrl;
+    protected String loginRedirectUrl;
+    protected Boolean selfService;
+    protected ApplicationSignOnMode signOnMode;
+    protected Boolean iOS;
+    protected Boolean web;
+
+
+    @Override
+    public T setName(String name) {
+        this.name = name;
+        return self();
+    }
+
+    @Override
+    public T setLabel(String label) {
+        this.label = label;
+        return self();
+    }
+
+    @Override
+    public T setErrorRedirectUrl(String errorRedirectUrl) {
+        this.errorRedirectUrl = errorRedirectUrl;
+        return self();
+    }
+
+    @Override
+    public T setLoginRedirectUrl(String loginRedirectUrl) {
+        this.loginRedirectUrl = loginRedirectUrl;
+        return self();
+    }
+
+    @Override
+    public T setSelfService(Boolean selfService) {
+        this.selfService = selfService;
+        return self();
+    }
+
+    @Override
+    public T setSignOnMode(ApplicationSignOnMode signOnMode) {
+        this.signOnMode = signOnMode;
+        return self();
+    }
+
+    @Override
+    public T setIOS(Boolean iOS) {
+        this.iOS = iOS;
+        return self();
+    }
+
+    @Override
+    public T setWeb(Boolean web) {
+        this.web = web;
+        return self();
+    }
+
+    @SuppressWarnings("unchecked")
+    protected T self() { return (T) this;}
+
+    @Override
+    public Application buildAndCreate(Client client) { return client.createApplication(build(client)); }
+
+    private Application build(Client client){
+
+        Application application = client.instantiate(Application.class);
+
+        if (Strings.hasText(name))
+            ((AbstractResource)application).setProperty("name", name, true);
+
+        if (Strings.hasText(label)) application.setLabel(label);
+
+        if (Objects.nonNull(signOnMode)) application.setSignOnMode(signOnMode);
+
+        // Accessibility
+        application.setAccessibility(client.instantiate(ApplicationAccessibility.class));
+        ApplicationAccessibility applicationAccessibility = application.getAccessibility();
+
+        if (Strings.hasText(loginRedirectUrl))
+            applicationAccessibility.setLoginRedirectUrl(loginRedirectUrl);
+
+        if (Strings.hasText(errorRedirectUrl))
+            applicationAccessibility.setErrorRedirectUrl(errorRedirectUrl);
+
+        if (Objects.nonNull(selfService))
+            applicationAccessibility.setSelfService(selfService);
+
+        // Visibility
+        application.setVisibility(client.instantiate(ApplicationVisibility.class));
+        ApplicationVisibility applicationVisibility = application.getVisibility();
+        ApplicationVisibilityHide applicationVisibilityHide = client.instantiate(ApplicationVisibilityHide.class);
+
+        if(Objects.nonNull(iOS))
+            applicationVisibility.setHide(applicationVisibilityHide
+                .setIOS(iOS));
+
+        if(Objects.nonNull(web))
+            applicationVisibility.setHide(applicationVisibilityHide
+                .setWeb(web));
+
+        return application;
+    }
+
+}
---
+++ integration-tests/src/test/groovy/com/okta/sdk/tests/it/OIdCApplicationIT.groovy
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2020-Present Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.sdk.tests.it
+
+import com.okta.sdk.client.Client
+import com.okta.sdk.resource.application.*
+import com.okta.sdk.tests.it.util.ITSupport
+
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.is
+
+class OIdCApplicationIT extends ITSupport implements CrudTestSupport{
+    @Override
+    def create(Client client) {
+        Application app = OIdCApplicationBuilder.instance()
+            .setName("oidc_client")
+            .setLabel("SDK test app - 1")
+            .addRedirectUris("http://www.google.com")
+            .setResponseTypes(Arrays.asList(OAuthResponseType.TOKEN, OAuthResponseType.CODE))
+            .setGrantTypes(Arrays.asList(OAuthGrantType.IMPLICIT, OAuthGrantType.AUTHORIZATION_CODE))
+            .setApplicationType(OpenIdConnectApplicationType.NATIVE)
+            .setClientId("dummy_id")
+            .setClientSecret("dummy_shgfhsgdhfgwg")
+            .setAutoKeyRotation(true)
+            .setTokenEndpointAuthMethod(OAuthEndpointAuthenticationMethod.NONE)
+            .setIOS(false)
+            .setWeb(true)
+            .setLoginRedirectUrl("http://www.myapp.com")
+            .setErrorRedirectUrl("http://www.myapp.com/errror")
+            .buildAndCreate(client);
+
+        return (OpenIdConnectApplication)app
+    }
+
+    @Override
+    def read(Client client, String id) {
+        return client.getApplication(id)
+    }
+
+    @Override
+    Iterator getResourceCollectionIterator(Client client) {
+        return client.listApplications().iterator()
+    }
+
+    @Override
+    void update(Client client, def application) {
+        application.setLabel("test_app")
+        application.visibility.hide.iOS = false
+        application.update()
+    }
+
+    @Override
+    void assertUpdate(Client client, def app) {
+        assertThat app.label, is("test_app")
+        assertThat app.visibility.hide.iOS, is(false)
+        assertThat app.credentials.oauthClient.client_id, is("dummy_id")
+
+    }
+}
