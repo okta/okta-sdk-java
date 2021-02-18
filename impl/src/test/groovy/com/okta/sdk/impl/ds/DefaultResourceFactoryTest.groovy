85491322685c942b1dfe7fdfbaa3edc86bcb54c2
Arvind Krishnakumar <arvind.krishnakumar@okta.com>
2021-02-18 13:58:17 -0800

    cleanup unused import

--- impl/src/test/groovy/com/okta/sdk/impl/ds/DefaultResourceFactoryTest.groovy
+++ impl/src/test/groovy/com/okta/sdk/impl/ds/DefaultResourceFactoryTest.groovy
@@ -15,7 +15,6 @@
  */
 package com.okta.sdk.impl.ds
 
-import com.okta.sdk.resource.Resource
 import com.okta.sdk.resource.user.factor.UserFactor
 import com.okta.sdk.resource.user.factor.FactorType
 import com.okta.sdk.resource.user.factor.TotpUserFactor
