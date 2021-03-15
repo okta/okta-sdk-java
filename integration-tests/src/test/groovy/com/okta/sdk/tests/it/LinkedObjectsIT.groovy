f9419e86db5c16fb3d57ccfb9d91aee7211ba4b6
Sergii Shamrai <71881713+sergiishamrai-okta@users.noreply.github.com>
2021-03-15 17:59:56 +0200

    CleanUp LinkedObjects

--- integration-tests/src/test/groovy/com/okta/sdk/tests/it/LinkedObjectsIT.groovy
+++ integration-tests/src/test/groovy/com/okta/sdk/tests/it/LinkedObjectsIT.groovy
@@ -198,4 +198,9 @@ class LinkedObjectsIT extends ITSupport {
         assertLinkedObjectPresent(client.listLinkedObjectDefinitions(), createdLinkedObjectDefinition1)
         assertLinkedObjectPresent(client.listLinkedObjectDefinitions(), createdLinkedObjectDefinition2)
     }
+
+    @Test
+    void cleanUpLinkedObjects() {
+        client.listLinkedObjectDefinitions().stream().forEach({ x -> x.delete() })
+    }
 }
