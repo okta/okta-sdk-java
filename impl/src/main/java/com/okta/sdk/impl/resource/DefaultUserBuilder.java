fe7588292aa34a694be8c6810f989f1c71dda02b
Sergii Shamrai <71881713+sergiishamrai-okta@users.noreply.github.com>
2021-03-17 11:06:05 +0200

    Imports updated

--- impl/src/main/java/com/okta/sdk/impl/resource/DefaultUserBuilder.java
+++ impl/src/main/java/com/okta/sdk/impl/resource/DefaultUserBuilder.java
@@ -18,8 +18,16 @@ package com.okta.sdk.impl.resource;
 import com.okta.sdk.client.Client;
 import com.okta.commons.lang.Collections;
 import com.okta.commons.lang.Strings;
-import com.okta.sdk.resource.user.*;
+import com.okta.sdk.resource.user.PasswordCredentialHook;
+import com.okta.sdk.resource.user.RecoveryQuestionCredential;
+import com.okta.sdk.resource.user.UserBuilder;
+import com.okta.sdk.resource.user.PasswordCredential;
+import com.okta.sdk.resource.user.User;
+import com.okta.sdk.resource.user.UserCredentials;
+import com.okta.sdk.resource.user.UserNextLogin;
+import com.okta.sdk.resource.user.UserProfile;
 import com.okta.sdk.resource.user.type.UserType;
+import com.okta.sdk.resource.user.CreateUserRequest;
 
 import java.util.Arrays;
 import java.util.HashMap;
