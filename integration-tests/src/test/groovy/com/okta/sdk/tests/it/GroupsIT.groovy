f3a3bd7fc65fc1fa45f7b453ff01a869e0a6f383
Vitalii Tytarenko <Vitalii.Tytarenko@okta.com>
2021-06-24 10:04:47 +0300

    OKTA-406629: increase sleep before search

--- integration-tests/src/test/groovy/com/okta/sdk/tests/it/GroupsIT.groovy
+++ integration-tests/src/test/groovy/com/okta/sdk/tests/it/GroupsIT.groovy
@@ -116,7 +116,7 @@ class GroupsIT extends ITSupport implements CrudTestSupport {
         validateGroup(group, groupName)
 
         // 2. Search the group by search parameter
-        Thread.sleep(500)
+        Thread.sleep(1000)
         assertPresent(client.listGroups(null, "profile.name eq \"" + groupName + "\"", null), group)
     }
 
