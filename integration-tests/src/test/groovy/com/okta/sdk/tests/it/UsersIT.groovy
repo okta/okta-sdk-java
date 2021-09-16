0815f824ef1f62400bf63b0f30f7af388a3892dd
Sergii Shamrai <71881713+sergiishamrai-okta@users.noreply.github.com>
2021-09-16 10:55:16 +0300

    UsersIT fixed

--- integration-tests/src/test/groovy/com/okta/sdk/tests/it/UsersIT.groovy
+++ integration-tests/src/test/groovy/com/okta/sdk/tests/it/UsersIT.groovy
@@ -841,7 +841,7 @@ class UsersIT extends ITSupport implements CrudTestSupport {
                 .setQuestion('How many roads must a man walk down?')
                 .setAnswer('forty two'))
 
-        ForgotPasswordResponse response = user.forgotPasswordSetNewPassword(userCredentials, false)
+        UserCredentials response = user.forgotPasswordSetNewPassword(userCredentials, false)
         assertThat response.get("recovery_question")["question"], equalTo("How many roads must a man walk down?")
         assertThat response.get("provider")["type"], equalTo("OKTA")
         assertThat response.get("provider")["name"], equalTo("OKTA")
