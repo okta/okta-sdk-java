3d0d25ef4ba57b3c30c12761044924229496e1c1
Arvind Krishnakumar <arvind.krishnakumar@okta.com>
2022-07-28 15:19:55 -0500

    fix pmd violation

--- impl/src/main/java/com/okta/sdk/impl/client/DefaultClientBuilder.java
+++ impl/src/main/java/com/okta/sdk/impl/client/DefaultClientBuilder.java
@@ -72,7 +72,6 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
