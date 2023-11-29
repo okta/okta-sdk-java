a19b982c02dee365545e56e5146fbce92e2f105d
Arvind Krishnakumar <arvind.krishnakumar@okta.com>
2023-11-29 11:42:17 -0600

    cleanup unused imports

--- impl/src/main/java/com/okta/sdk/impl/resource/DefaultUserBuilder.java
+++ impl/src/main/java/com/okta/sdk/impl/resource/DefaultUserBuilder.java
@@ -39,8 +39,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DefaultUserBuilder implements UserBuilder {
 
