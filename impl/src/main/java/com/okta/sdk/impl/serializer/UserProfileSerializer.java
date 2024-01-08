f8b6901a8ef80b0bc3e88d076e5e2250cad84d62
Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>
2024-01-08 09:37:50 -0600

    Fix issue #968 - user profile serializer sending null values (#969)

--- impl/src/main/java/com/okta/sdk/impl/serializer/UserProfileSerializer.java
+++ impl/src/main/java/com/okta/sdk/impl/serializer/UserProfileSerializer.java
@@ -66,32 +66,109 @@ public class UserProfileSerializer extends StdSerializer<UserProfile> {
 
         // nullable fields
 
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_CITY, userProfile.getCity());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_COST_CENTER, userProfile.getCostCenter());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_COUNTRY_CODE, userProfile.getCountryCode());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_DEPARTMENT, userProfile.getDepartment());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_DISPLAY_NAME, userProfile.getDisplayName());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_DIVISION, userProfile.getDivision());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_EMPLOYEE_NUMBER, userProfile.getEmployeeNumber());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_HONORIFIC_PREFIX, userProfile.getHonorificPrefix());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_HONORIFIC_SUFFIX, userProfile.getHonorificSuffix());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_MANAGER, userProfile.getManager());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_MANAGER_ID, userProfile.getManagerId());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_MIDDLE_NAME, userProfile.getMiddleName());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_MOBILE_PHONE, userProfile.getMobilePhone());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_NICK_NAME, userProfile.getNickName());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_ORGANIZATION, userProfile.getOrganization());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_POSTAL_ADDRESS, userProfile.getPostalAddress());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_PREFERRED_LANGUAGE, userProfile.getPreferredLanguage());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_PRIMARY_PHONE, userProfile.getPrimaryPhone());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_PROFILE_URL, userProfile.getProfileUrl());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_SECOND_EMAIL, userProfile.getSecondEmail());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_STATE, userProfile.getState());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_STREET_ADDRESS, userProfile.getStreetAddress());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_TIMEZONE, userProfile.getTimezone());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_TITLE, userProfile.getTitle());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_USER_TYPE, userProfile.getUserType());
-        jgen.writeStringField(UserProfile.JSON_PROPERTY_ZIP_CODE, userProfile.getZipCode());
+        if (Strings.hasText(userProfile.getCity())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_CITY, userProfile.getCity());
+        }
+
+        if (Strings.hasText(userProfile.getCountryCode())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_COUNTRY_CODE, userProfile.getCountryCode());
+        }
+
+        if (Strings.hasText(userProfile.getDepartment())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_DEPARTMENT, userProfile.getDepartment());
+        }
+
+        if (Strings.hasText(userProfile.getDisplayName())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_DISPLAY_NAME, userProfile.getDisplayName());
+        }
+
+        if (Strings.hasText(userProfile.getDivision())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_DIVISION, userProfile.getDivision());
+        }
+
+        if (Strings.hasText(userProfile.getCostCenter())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_COST_CENTER, userProfile.getCostCenter());
+        }
+
+        if (Strings.hasText(userProfile.getEmployeeNumber())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_EMPLOYEE_NUMBER, userProfile.getEmployeeNumber());
+        }
+
+        if (Strings.hasText(userProfile.getHonorificPrefix())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_HONORIFIC_PREFIX, userProfile.getHonorificPrefix());
+        }
+
+        if (Strings.hasText(userProfile.getHonorificSuffix())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_HONORIFIC_SUFFIX, userProfile.getHonorificSuffix());
+        }
+
+        if (Strings.hasText(userProfile.getManager())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_MANAGER, userProfile.getManager());
+        }
+
+        if (Strings.hasText(userProfile.getManagerId())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_MANAGER_ID, userProfile.getManagerId());
+        }
+
+        if (Strings.hasText(userProfile.getMiddleName())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_MIDDLE_NAME, userProfile.getMiddleName());
+        }
+
+        if (Strings.hasText(userProfile.getMobilePhone())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_MOBILE_PHONE, userProfile.getMobilePhone());
+        }
+
+        if (Strings.hasText(userProfile.getNickName())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_NICK_NAME, userProfile.getNickName());
+        }
+
+        if (Strings.hasText(userProfile.getOrganization())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_ORGANIZATION, userProfile.getOrganization());
+        }
+
+        if (Strings.hasText(userProfile.getPostalAddress())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_POSTAL_ADDRESS, userProfile.getPostalAddress());
+        }
+
+        if (Strings.hasText(userProfile.getPreferredLanguage())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_PREFERRED_LANGUAGE, userProfile.getPreferredLanguage());
+        }
+
+        if (Strings.hasText(userProfile.getPrimaryPhone())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_PRIMARY_PHONE, userProfile.getPrimaryPhone());
+        }
+
+        if (Strings.hasText(userProfile.getProfileUrl())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_PROFILE_URL, userProfile.getProfileUrl());
+        }
+
+        if (Strings.hasText(userProfile.getSecondEmail())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_SECOND_EMAIL, userProfile.getSecondEmail());
+        }
+
+        if (Strings.hasText(userProfile.getState())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_STATE, userProfile.getState());
+        }
+
+        if (Strings.hasText(userProfile.getStreetAddress())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_STREET_ADDRESS, userProfile.getStreetAddress());
+        }
+
+        if (Strings.hasText(userProfile.getTimezone())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_TIMEZONE, userProfile.getTimezone());
+        }
+
+        if (Strings.hasText(userProfile.getTitle())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_TITLE, userProfile.getTitle());
+        }
+
+        if (Strings.hasText(userProfile.getUserType())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_USER_TYPE, userProfile.getUserType());
+        }
+
+        if (Strings.hasText(userProfile.getZipCode())) {
+            jgen.writeStringField(UserProfile.JSON_PROPERTY_ZIP_CODE, userProfile.getZipCode());
+        }
 
         Map<String, Object> additionalProperties = userProfile.getAdditionalProperties();
 
