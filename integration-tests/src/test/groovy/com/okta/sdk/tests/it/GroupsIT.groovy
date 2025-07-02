fb7a9d1fad8bfeb00fb2f73d3e06c25923b7a34d
Prachi Pandey <prachi.pandey@okta.com>
2025-07-02 14:02:26 +0530

    adding delay for testcase

--- integration-tests/src/test/groovy/com/okta/sdk/tests/it/GroupsIT.groovy
+++ integration-tests/src/test/groovy/com/okta/sdk/tests/it/GroupsIT.groovy
@@ -121,7 +121,7 @@ class GroupsIT extends ITSupport {
         validateGroup(group, groupNameUpdated, 'Description updated')
     }
 
-    @Test (groups = "group1")
+    @Test(groups = "group1")
     @Scenario("group-user-operations")
     void groupUserOperationsTest() {
 
@@ -151,12 +151,15 @@ class GroupsIT extends ITSupport {
 
         // 2. Add user to the group and validate user present in group
         groupApi.assignUserToGroup(group.getId(), user.getId())
+        Thread.sleep(getTestOperationDelay())
 
         assertUserInGroup(user, group, groupApi, 5, getTestOperationDelay())
 
         // 3. Remove user from group and validate user removed
         groupApi.unassignUserFromGroup(group.getId(), user.getId())
+        Thread.sleep(getTestOperationDelay())
 
-        assertUserNotInGroup(user, group, groupApi,5, getTestOperationDelay())
+        assertUserNotInGroup(user, group, groupApi, 5, getTestOperationDelay())
     }
+
 }
